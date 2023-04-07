0ae74c73f66958a7aa3fe4d383bb3b139297fdcb
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-04-08 02:18:30 +0300

    home component navbar and logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Icon from '../img/icons8-pizza-48.png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'> 
+      <div className="logo-navbar-container">
+        <div className="logo">
+          <img src={Icon} alt="icon" />
+          <span>pizza mania</span>
+        </div>
+        <div className="navbar">
+          <ul>
+            <li>home</li>
+            <li>menu</li>
+            <li>spesials & coupos</li>
+            <li>contact us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
